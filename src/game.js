87830a4f7a1e8ba6f225e5b6ec367112c1f4a135
Bailey Stoner <monokrome@monokro.me>
2016-10-27 17:07:18 -0700

    To OOP or not to OOP?

--- src/game.js
+++ src/game.js
@@ -1,29 +1,64 @@
-const scene = new THREE.Scene(),
-      renderer = new THREE.WebGLRenderer(),
-      camera = new THREE.PerspectiveCamera(
-            75, window.innerWidth / window.innerHeight, 0.1, 1000
-      ),
-
-     geometry = new THREE.BoxGeometry(1, 1, 1),
-     material = new THREE.MeshBasicMaterial({color: 0xff9900}),
-     cube = new THREE.Mesh(geometry, material);
+(function (THREE, window, undefined) {
 
 
-scene.add(camera);
-scene.add(cube);
+class Game {
+      constructor(root) {
+            this.renderer = this.getRenderer(root);
+            this.scene = this.getScene();
+      }
 
+      getRenderer(root) {
+            const renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            root.appendChild(renderer.domElement);
+            return renderer;
 
-camera.position.z = 5;
+      }
 
+      getCamera() {
+            const camera = new THREE.PerspectiveCamera(
+                        75, window.innerWidth / window.innerHeight,
+                        0.1, 1000
+                  );
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+            camera.position.z = 5;
+            return camera;
+      }
 
-function render() {
-      cube.rotation.x = cube.rotation.y += 0.01;
+      getCube() {
+            const geometry = new THREE.BoxGeometry(1, 1, 1),
+                  material = new THREE.MeshBasicMaterial({
+                        color: 0xff9900,
+                  });
 
-      renderer.render(scene, camera);
-      requestAnimationFrame(render);
+            return new THREE.Mesh(geometry, material);
+      }
+
+      getScene() {
+            const scene = new THREE.Scene();
+
+            this.cube = this.getCube();
+            this.camera = this.getCamera();
+
+            scene.add(this.camera);
+            scene.add(this.cube);
+
+            return scene;
+      }
+
+      start() {
+            this.render = this.render.bind(this);
+            this.render();
+      }
+
+      render() {
+            this.cube.rotation.x = this.cube.rotation.y += 0.01;
+            this.renderer.render(this.scene, this.camera);
+            requestAnimationFrame(this.render);
+      }
 }
 
-render();
+(new Game(document.body)).start();
+
+
+})(THREE, window);
